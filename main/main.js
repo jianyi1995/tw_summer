f16b312c1c48cd912adb3c5e6c9a9fda82fbfe8f
shaowenhan <1138500211@qq.com>
2017-05-18 22:31:18 +0800

    I do not know where I am wrong

--- main/main.js
+++ main/main.js
@@ -1,4 +1,8 @@
 module.exports = function main() {
 	const Person = require('./person.js')
 	const Student = require('./student.js')
+	var p = new Person('Tom', 21)
+	console.log(p.introduce())
+	var s = new Student('Tom', 21, 2)
+	console.log(s.introduce())
 };
