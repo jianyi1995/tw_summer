235abfbb5ae2e9eb6d2631f7f5bbbfc1c4f06e7c
shaowenhan <1138500211@qq.com>
2017-05-18 21:16:00 +0800

    I do not know

--- main/main.js
+++ main/main.js
@@ -1,3 +1,4 @@
 module.exports = function main() {
-	
+	const Person = require('./person.js')
+	const Student = require('./student.js') 
 };
--- main/student.js
+++ main/student.js
@@ -1,4 +1,14 @@
-import * from './person.js'
+class Person{
+	constructor(name, age)
+	{
+		this.name = name
+		this.age = age
+	}
+	introduce(){
+		return'My name is ' + this.name + '. I am ' +
+			this.age + ' years old.'
+	}
+}
 
 class Student extends Person{
 	constructor(name, age, klass){
