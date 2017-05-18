3b24fd3f99836eac5b977676d059f897dc4d672e
shaowenhan <1138500211@qq.com>
2017-05-18 19:41:40 +0800

    make float to integer

--- main/main.js
+++ main/main.js
@@ -1,10 +1,10 @@
 module.exports = function main(s) {
 	var number = parseInt(s)
 	var res = 0
-	while(number > 0)
+	while(number != 0)
 	{
 		res += number % 10
-		number /= 10
+		number = (number - number % 10) / 10
 	}
 	return res
 };
