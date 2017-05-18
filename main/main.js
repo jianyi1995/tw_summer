4f985aa9127f58e30028956b4af823402870cac2
shaowenhan <1138500211@qq.com>
2017-05-18 22:48:49 +0800

    a error because of .

--- main/main.js
+++ main/main.js
@@ -1,4 +1,6 @@
 module.exports = function(){
 	let Person = require('./person.js')
 	let Student = require('./student.js')
+	var s = new Student('Tom', 21, 2)
+	console.log(s.introduce())
 }
--- main/student.js
+++ main/student.js
@@ -7,7 +7,7 @@ class Student extends Person{
 	introduce(){
 		return 'My name is ' + this.name + '. I am ' +
 			this.age + ' years old. I am a Student. I am at Class '
-			+ this.klass + '. '
+			+ this.klass + '.'
 	}
 }
 module.exports = Student
